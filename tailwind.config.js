b2381e37127f58c3342001bf33f05dc40527136e
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-03-25 22:13:55 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        default: ["14px", { lineHeight: "18px" }],
+        xs: ["16px", { lineHeight: "22px" }],
+        xl: ["22px", { lineHeight: "32px" }],
+      },
+      colors: {},
+      backgroundImage: {},
+    },
+    screens: {
+      mini: "520px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "15px",
+        sm: "20px",
+        lg: "40px",
+        xl: "60px",
+      },
+    },
+  },
+  plugins: [flowbite.plugin()],
+};
