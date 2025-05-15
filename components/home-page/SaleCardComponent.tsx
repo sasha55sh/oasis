4d13efd56613c492048a2d49d2e9eb8fe51cd49e
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-05-15 18:43:25 +0300

    Create SaleCardComponent.tsx

---
+++ components/home-page/SaleCardComponent.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import Image from "next/image";
+
+interface cardProps {
+  className?: string;
+  title: string;
+  discount: string;
+  imageScr: string;
+  imageAlt: string;
+}
+const SaleCardComponent: FC<cardProps> = ({
+  className,
+  title,
+  discount,
+  imageAlt,
+  imageScr,
+}) => {
+  return (
+    <div className={`${className} relative group`}>
+      <Image src={imageScr} alt={imageAlt} />
+
+      <div className="absolute inset-0 flex flex-col *:capitalize items-center justify-center space-y-[20px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 *:rounded-md">
+        <p className="text-amberOrange py-[5px] px-[15px] bg-white">
+          {discount}
+        </p>
+        <button className="hover:text-amberOrange px-[25px] py-[10px] hover:bg-white bg-amberOrange text-white">
+          {title}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SaleCardComponent;
