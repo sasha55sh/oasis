ab1421317cc3ba26beafa6cff960d7b7d0a963c1
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-05-17 18:34:20 +0300

    Update TestimonialComponent.tsx

--- components/home-page/TestimonialComponent.tsx
+++ components/home-page/TestimonialComponent.tsx
@@ -22,16 +22,19 @@ const TestimonialComponent: FC<testimonialProps> = ({
 }) => {
   return (
     <div
-      className={`${className} bg-white py-[30px] px-[60px] rounded-lg relative`}
+      className={`${className} relative bg-white py-[30px] px-[60px] rounded-lg`}
     >
-      <Image src={flowersBg} alt="flowers background" className="adsolute" />
 
-      <div className="adsolute flex flex-col items-center space-y-[20px]">
+      <div className="-mt-[60px] z-10 adsolute flex flex-col items-center space-y-[20px]">
         <Image src={reviewerSrc} alt="reviewer photo" />
         <Image src={quotes} alt="quotes" />
-        <p className="text-[18px] text-darkLiver text-center max-w-[700px]">{responseText}</p>
+        <p className="text-[18px] text-darkLiver text-center max-w-[700px]">
+          {responseText}
+        </p>
         <Image src={ratingStars} alt="rating stars" />
-        <p className="text-[24px] text-darkCharcoal font-bold">{reviewerName}</p>
+        <p className="text-[24px] text-darkCharcoal font-bold">
+          {reviewerName}
+        </p>
         <p className="text-[16px] text-oldSilver">{reviewerOccupation}</p>
       </div>
     </div>
