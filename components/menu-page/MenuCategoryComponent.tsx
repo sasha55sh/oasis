37cdd9759db5a2e9dd651e5eb199365ea6d9ed00
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-05-30 18:53:24 +0300

    Update MenuCategoryComponent.tsx

--- components/menu-page/MenuCategoryComponent.tsx
+++ components/menu-page/MenuCategoryComponent.tsx
@@ -1,19 +1,38 @@
 import React, { FC } from "react";
 import Image from "next/image";
 import { Menu } from "@/config/types";
+import MenuItemComponent from "./MenuItemComponent";
 import CoffeeIcon from "@/images/menu-page/coffee-icon.svg";
 
 interface menuProps {
   className?: string;
-  menu: Menu;
+  sectionTitle: string;
+  imageSrc: string;
+  imageAlt: string;
+  items: Menu[];
 }
 
-const MenuCategoryComponent: FC<menuProps> = ({ className, menu }) => {
+const MenuCategoryComponent: FC<menuProps> = ({
+  className,
+  sectionTitle,
+  items,
+  imageSrc,
+  imageAlt,
+}: menuProps) => {
   return (
-    <div className="bg-darkCharcoal">
-      <Image src={CoffeeIcon} alt="Coffee icon" />
-      <h1 className="font-bold text-[48px]">{menu.category}</h1>
-      
+    <div className={`${className} flex justify-between`}>
+      <Image src={imageSrc} alt={imageAlt} />
+      <div>
+        <Image src={CoffeeIcon} alt="coffe icon" />
+        <h1 className="text-[48px] text-darkCharcoal font-bold">
+          {sectionTitle}
+        </h1>
+        <div className="flex flex-col space-y-[10px]">
+          {items.map((item, index) => (
+            <MenuItemComponent key={index} item={item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
