8983bd020533ee9e95ead4cdb149e219b8302926
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-07-02 13:15:10 +0300

    Update types.ts

--- config/types.ts
+++ config/types.ts
@@ -6,3 +6,48 @@ export type Menu = {
   category: "starter menu" | "main course" | "dessert" | "drinks";
   price: number;
 };
+
+export type Faq = {
+  id: string;
+  question: string;
+  answer: string;
+};
+
+export type CardProps = {
+  id: string;
+  handle: string;
+  title: string;
+  price: number;
+  discount: number;
+  image: string;
+  gramm: number;
+  quantity: number;
+};
+
+export type CartProductProps = {
+  id: string;
+  handle: string;
+  title: string;
+  price: number;
+  image: string;
+};
+
+export type InfoMessage = {
+  type: "success" | "error";
+  text: string;
+};
+
+export type Product = {
+  id: string;
+  title: string;
+  handle: string;
+  grams: number;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  proteins: number;
+  carbohydrates: number;
+  fats: number;
+  cal: number;
+};
