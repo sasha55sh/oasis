0bfcd36d0e8b1da85fda542c889fa85d1c8caf3e
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-03 15:08:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
