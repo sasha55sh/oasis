b15430058b5ab4d58255cda51e7340e6f6afc9bc
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-04 13:38:38 +0300

    production settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
+  output: "export",
   images: {
+    unoptimized: true,
     domains: ["res.cloudinary.com"],
   },
 };
