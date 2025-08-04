ee3c8e7512e1bfc851f717a36426f25b523af08f
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-04 14:18:11 +0300

    Revert "production settings"
    This reverts commit b15430058b5ab4d58255cda51e7340e6f6afc9bc.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  output: "export",
   images: {
-    unoptimized: true,
     domains: ["res.cloudinary.com"],
   },
 };
