9267a3b31fe790fa87e7a563e91089c086dae812
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-06 12:49:34 +0300

    refactor: only logged user can add to favorite

--- components/shop-page/CardComponent.tsx
+++ components/shop-page/CardComponent.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import { Card } from "flowbite-react";
 import { CardProps } from "@/config/types";
 import { useCart } from "@/hooks/useCart";
+import { toggleFavorite } from "@/service/UserService";
 import Counter from "@/components/CounterComponent";
+import AuthModal from "@/components/account-page/LoginComponent";
 
 import Heart from "@/images/vectors/heart-icon.svg";
 import HeartFilled from "@/images/vectors/filled-heard-icon.svg";
 import Plus from "@/images/vectors/plus-icon.svg";
-import { toggleFavorite } from "@/service/UserService";
 
 const CardComponent: FC<
   CardProps & {
@@ -30,8 +31,10 @@ const CardComponent: FC<
   initialFavorite = false,
   onFavoriteChange,
 }) => {
-  const { addToCart, products, removeFromCart } = useCart();
   const [isFavorite, setIsFavorite] = useState(initialFavorite);
+  const [openModal, setOpenModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { addToCart, products, removeFromCart } = useCart();
 
   const cartProduct = products.find((p) => p.id === id);
   const quantity = cartProduct?.quantity || 0;
@@ -39,8 +42,13 @@ const CardComponent: FC<
 
   useEffect(() => setIsFavorite(initialFavorite), [initialFavorite]);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
+
   const handleToggleFavorite = async () => {
-    try {
+    if (isLoggedIn) {
       await toggleFavorite({
         id,
         handle,
@@ -51,13 +59,14 @@ const CardComponent: FC<
         description,
         discount,
       });
+
       setIsFavorite(!isFavorite);
 
       if (onFavoriteChange) {
-        onFavoriteChange(); // Повідомляємо батьківський компонент
+        onFavoriteChange();
       }
-    } catch (error) {
-      console.error("Failed to toggle favorite", error);
+    } else {
+      setOpenModal(true);
     }
   };
 
@@ -85,81 +94,87 @@ const CardComponent: FC<
   ).toFixed(2);
 
   return (
-    <Card
-      className={`${className} max-w-sm relative`}
-      renderImage={() => (
-        <Image
-          width={385}
-          height={385}
-          src={image}
-          alt={handle}
-          priority
-          className="h-[380px] rounded-t-lg object-cover"
-        />
-      )}
-    >
-      {discount > 0 && (
-        <span className="absolute p-[10px] bg-electricRed text-white top-0 left-0 rounded-xl">
-          -{Number(discount) * 100}%
-        </span>
-      )}
+    <>
+      <AuthModal openModal={openModal} setOpenModal={setOpenModal} />
+      <Card
+        className={`${className} max-w-sm relative`}
+        renderImage={() => (
+          <Image
+            width={385}
+            height={385}
+            src={image}
+            alt={handle}
+            priority
+            className="h-[380px] rounded-t-lg object-cover"
+          />
+        )}
+      >
+        {discount > 0 && (
+          <span className="absolute p-[10px] bg-electricRed text-white top-0 left-0 rounded-xl">
+            -{Number(discount) * 100}%
+          </span>
+        )}
 
-      <a href={`/shop/${handle}`} className="flex flex-col space-y-[10px]">
-        <h5 className="text-[22px] text-darkLiver font-bold leading-none h-[45px]">
-          {title}
-        </h5>
-        <p className="text-amberOrange">{grams} g</p>
-        <p className="text-oldSilver h-[80px]">{description}</p>
-      </a>
+        <a href={`/shop/${handle}`} className="flex flex-col space-y-[10px]">
+          <h5 className="text-[22px] text-darkLiver font-bold leading-none h-[45px]">
+            {title}
+          </h5>
+          <p className="text-amberOrange">{grams} g</p>
+          <p className="text-oldSilver h-[80px]">{description}</p>
+        </a>
 
-      <div className="flex justify-between items-center">
-        {discount !== 0 ? (
-          <div className="flex gap-[10px] items-center">
-            <p className="text-[24px] text-electricRed font-bold">
-              {discountedPrice} $
-            </p>
-            <p className="text-amberOrange font-medium line-through">
+        <div className="flex justify-between items-center">
+          {discount !== 0 ? (
+            <div className="flex gap-[10px] items-center">
+              <p className="text-[24px] text-electricRed font-bold">
+                {discountedPrice} $
+              </p>
+              <p className="text-amberOrange font-medium line-through">
+                {Number(price).toFixed(2)} $
+              </p>
+            </div>
+          ) : (
+            <p className="text-[24px] text-amberOrange font-bold">
               {Number(price).toFixed(2)} $
             </p>
-          </div>
-        ) : (
-          <p className="text-[24px] text-amberOrange font-bold">
-            {Number(price).toFixed(2)} $
-          </p>
-        )}
+          )}
 
-        {quantity > 0 ? (
-          <Counter
-            quantity={quantity}
-            onIncrease={handleIncrease}
-            onDecrease={handleDecrease}
-            maxQuantity={maxQuantity}
-          />
-        ) : (
-          <div className="flex gap-[10px]">
-            <button
-              onClick={handleToggleFavorite}
-              className="p-[10px] rounded-xl bg-oldSilver/10 w-[56px] h-[56px] flex items-center justify-center"
-              aria-label="like-button"
-            >
-              <Image src={isFavorite ? HeartFilled : Heart} alt="heart-icon" />
-            </button>
+          {quantity > 0 ? (
+            <Counter
+              quantity={quantity}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+              maxQuantity={maxQuantity}
+            />
+          ) : (
+            <div className="flex gap-[10px]">
+              <button
+                onClick={handleToggleFavorite}
+                className="p-[10px] rounded-xl bg-oldSilver/10 w-[56px] h-[56px] flex items-center justify-center"
+                aria-label="like-button"
+              >
+                <Image
+                  src={isFavorite ? HeartFilled : Heart}
+                  alt="heart-icon"
+                />
+              </button>
 
-            <button
-              className="w-[56px] h-[56px] rounded-xl flex items-center justify-center bg-amberOrange/50 hover:bg-amberOrange transition-colors group"
-              onClick={handleAddToCart}
-              aria-label="add-to-cart"
-            >
-              <Image
-                src={Plus}
-                alt="Plus Icon"
-                className="transition-colors group-hover:filter group-hover:brightness-0 group-hover:invert"
-              />
-            </button>
-          </div>
-        )}
-      </div>
-    </Card>
+              <button
+                className="w-[56px] h-[56px] rounded-xl flex items-center justify-center bg-amberOrange/50 hover:bg-amberOrange transition-colors group"
+                onClick={handleAddToCart}
+                aria-label="add-to-cart"
+              >
+                <Image
+                  src={Plus}
+                  alt="Plus Icon"
+                  className="transition-colors group-hover:filter group-hover:brightness-0 group-hover:invert"
+                />
+              </button>
+            </div>
+          )}
+        </div>
+      </Card>
+    </>
   );
 };
 
